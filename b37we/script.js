335a5e5e04567fb8d7246b3ed2dc72bf3a28a66f
ruban67 <52911922+ruban67@users.noreply.github.com>
2022-06-26 16:40:57 +0530

    Add files via upload

---
+++ b37we/script.js
@@ -0,0 +1,14 @@
+var array=[{
+    "id":"1",
+    "name":"ruban",
+    "email":"ruban@gmail.com"
+    },
+    {
+    id:"2",
+       name:"aaaa",
+    email:"aaaa@gmail.com"
+    }];
+    for(var i in array){
+    console.log(i,array[i].name);
+    }
+ 
---
+++ for/script.js
@@ -0,0 +1,19 @@
+var array=[{
+   "id":"1",
+   "name":"ruban",
+   "email":"ruban@gmail.com"
+   },
+   {
+      id:"2",
+      name:"aaaa",
+      email:"aaaa@gmail.com"
+   },
+    {
+      id:"3",
+      name:"xxxx",
+      email:"xxxx@gmail.com"
+    }];
+
+   for(var i=0;i<3;i++){
+      console.log(array[i]["id"],array[i].name,array[i].email);
+   }
